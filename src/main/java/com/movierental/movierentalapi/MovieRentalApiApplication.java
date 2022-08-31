d69a136e7045a174cfc9c2213f8b4fd218f1a1c0
Jasmyne Jean-Remy <jasmyne.jean-remy@publicissapient.com>
2022-08-31 10:06:15 -0400

    init

---
+++ src/main/java/com/movierental/movierentalapi/MovieRentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.movierental.movierentalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movierental/movierentalapi/MovieRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movierental.movierentalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
